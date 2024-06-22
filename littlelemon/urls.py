8fe86425b6d2b8c59701e2a174f6b3e5cae6279b
Gonzalo Larrica <76012483+goncho09@users.noreply.github.com>
2024-06-22 20:30:12 -0300

    Update urls.py

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('restaurant.urls')),
+    path('api/auth/', include('djoser.urls')),
 ]
